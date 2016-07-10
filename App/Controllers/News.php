b8e7446b53db87f44df1add1ada1dcdb37d65923
JamesPont <godundmytro@gmail.com>
2016-07-10 22:30:12 +0300

    extend controller

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -23,12 +23,18 @@ class News {
         return $this->$methodName();
     }
     protected function beforeAction(){
-        echo 'Counter';
+//        echo 'Counter';
     }
     public function actionIndex(){
 
         $this->view->title = 'Мой крутой сайт';
         $this->view->news = \App\Models\News::findAll();
         $this->view->display(__DIR__ . '/../templates/index.php');
+    }
+    
+    protected function ActionOne(){
+        $id = (int)$_GET['id'];
+        $this->view->article = \App\Models\News::findById($id);
+        $this->view->display(__DIR__ . '/../templates/one.php');
     }
 }

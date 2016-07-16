57648b6185795983f389af590cb8a06d86cc97ef
JamesPont <godundmytro@gmail.com>
2016-07-16 18:19:04 +0300

    install package "pr-of-it/t4" from composer

--- autoload.php
+++ autoload.php
@@ -5,18 +5,18 @@
 //}
 
 
-function my_app_autoload($class){
-    $filename = __DIR__ . '/' . str_replace('\\','/' , $class). '.php';
-    if(file_exists($filename)){
+function my_app_autoload($class) {
+    $filename = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($filename)) {
         include $filename;
     }
 }
 
 spl_autoload_register('my_app_autoload');
-spl_autoload_register(function($class){
-    $filename = __DIR__ . '/' . str_replace(['\\','App'] ,['/','lib'], $class. '.php' );
+spl_autoload_register(function ($class) {
+    $filename = __DIR__ . '/' . str_replace(['\\', 'App'], ['/', 'lib'], $class . '.php');
 
-    if(file_exists($filename)){
+    if (file_exists($filename)) {
         include $filename;
     }
 });
